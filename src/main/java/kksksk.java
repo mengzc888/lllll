30eda4ee7be7a5754b4a4cd20c8bf782349cc9d2
mengzc <mengzc163@163.com>
2021-03-21 16:08:23 +0800

    work0111

---
+++ src/main/java/kksksk.java
@@ -0,0 +1,10 @@
+/**
+ * @author Mengzc
+ * @create 2021-03-21 16:07
+ */
+public class kksksk {
+
+    public static void main(String[] args) {
+        System.out.println("ooo");
+    }
+}
--- src/main/java/work01.java
+++ src/main/java/work01.java
@@ -5,6 +5,6 @@
 public class work01 {
     public static void main(String[] args) {
         System.out.println("wo");
-        System.out.println("wo");
+        System.out.println("wo1");
     }
 }
